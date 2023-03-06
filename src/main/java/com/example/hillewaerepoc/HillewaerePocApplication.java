a19388ec630c2112be1b3d8c8ac1d3f5001ff8b4
Simon Masschelein <simon.masschelein@gmail.com>
2023-03-06 13:27:39 +0100

    Initial commit

---
+++ src/main/java/com/example/hillewaerepoc/HillewaerePocApplication.java
@@ -0,0 +1,13 @@
+package com.example.hillewaerepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillewaerePocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HillewaerePocApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/hillewaerepoc/HillewaerePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hillewaerepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HillewaerePocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
